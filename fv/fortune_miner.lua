68be46d374f686dd81de2d53314a441f1b26b5c2
TowardtheStars <hty16@mail.ustc.edu.cn>
2019-07-22 23:20:11 +0800

    修复了不能自动获取矿石的bug，将输入输出的方向和修工具的方块入口抽象化

--- fv/fortune_miner.lua
+++ fv/fortune_miner.lua
@@ -1,7 +1,7 @@
 -- @Author: FVortex
 -- @Date:   2019-07-22 18:54:54
 -- @Last Modified by:   TowardtheStars
--- @Last Modified time: 2019-07-22 23:02:07
+-- @Last Modified time: 2019-07-22 23:18:26
 
 local component = require("component")
 -- using openOS
@@ -10,6 +10,13 @@ local sides = require("sides")
 local inv = component.inventory_controller
 
 local ore_list = {"minecraft:diamond_ore","galacticraftcore:basic_block_core","galacticraftplanets:asteroids_block"}
+
+local repair_tool_side = sides.up
+local ore_src_side = sides.down
+local output_side = sides.up
+local max_use = 300
+
+
 local function ore_slot()
 	for i = 1,16 do
 		local item_stack = inv.getStackInternalSlot(i)
@@ -18,17 +25,17 @@ local function ore_slot()
 			return i
 		end
 	end
-
+	return nil
 end
 
 local function repair_tool()
 	inv.equip()
-	inv.dropIntoSlot(sides.up, 1)
+	inv.dropIntoSlot(repair_tool_side, 1)
 	inv.equip()
 end
 
 local use_count = 0
-local max_use = 300
+
 local function mine():
 	robot.swing()
 	robot.suck()
@@ -45,8 +52,14 @@ local output():
 		local item_stack = inv.getStackInternalSlot(i)
 		if item_stack and (not (item_stack.name in ore_list))
 		then
-			inv.dropIntoSlot(sides.up, 1)
+			inv.dropIntoSlot(output_side, 1)
 		end
+end
+
+local input_ore():
+	for i = 1, inv.getInventorySize(ore_src_side) do
+		inv.suckFromSlot(ore_src_side, i)
+	end
 end
 
 while (true)
@@ -57,7 +70,12 @@ do
 		mine()
 		output()
 	else
-		robot.select(ore_slot())
-		robot.place()
+		slot = ore_slot()
+		if slot then
+			robot.select(slot)
+			robot.place()
+		else
+			input_ore()
+		end
 	end
 end
