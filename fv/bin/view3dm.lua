2d4bc1a8fa0a7b3b0f2053eca6f87b1fc5068301
TowardtheStars <hty16@mail.ustc.edu.cn>
2019-07-27 17:33:03 +0800

    view3dm增加了一些功能

--- fv/bin/view3dm.lua
+++ fv/bin/view3dm.lua
@@ -1,7 +1,7 @@
 -- @Author: FVortex
 -- @Date:   2019-07-27 16:28:45
 -- @Last Modified by:   TowardtheStars
--- @Last Modified time: 2019-07-27 16:55:25
+-- @Last Modified time: 2019-07-27 17:32:41
 
 local component = require("component")
 local shell = require("shell")
@@ -27,7 +27,7 @@ if #addresses > 1 then
 end
 
 local hologram = component.hologram
-local args, ops = shell.parse({...})
+local args, ops = shell.parse(...)
 
 if #args < 1 then
   io.write("Usage: view3dm FILE [count]\n")
@@ -53,20 +53,55 @@ data = data()
 -----------------------------------------------------------
 io.write("Loading shapes...\n")
 
-local color_count = 0
-for i, shape in ipairs(data.shapes or {}) do
-    -- Get color settings from shell or automatically allocate colors to shapes
-    local color = ops[shape.texture]
-    if not color then
-        color = color_count
-        color_count = color_count + 1
-    end
-    for x = shape[1], shape[4]-1 do
-        for z = shape[3], shape[6]-1 do
-            hologram.fill(x, z, shape[2], shape[5]-1, color)
+
+local off_x = ops.x or 1
+local off_y = ops.y or 1
+local off_z = ops.z or 1
+
+local loop = ops.l
+local state = ops.t or false
+local uni_color = ops.u
+
+local function spc_check(i, c)
+    hologram.setPaletteColor(i, c or hologram.getPaletteColor(i))
+end
+
+spc_check(1, ops.c1)
+spc_check(2, ops.c2)
+spc_check(3, ops.c3)
+
+local function showHolo(state)
+    local color_count = 1
+    hologram.clear()
+    for i, shape in ipairs(data.shapes or {}) do
+        if shape.state == state then
+            -- Get color settings from shell or automatically allocate colors to shapes
+            local color = ops[shape.texture]
+            if not color then
+                color = color_count
+                color_count = (color_count + 1) % 3
+            end
+            for x = shape[1], shape[4]-1 do
+                for z = shape[3], shape[6]-1 do
+                    hologram.fill(x, z, shape[2], shape[5]-1, uni_color or color + 1)
+                end
+            end
         end
     end
 end
+
+hologram.setTransformation(off_x, off_y, off_z)
+
+if loop then
+    while true do
+        state = not state
+        showHolo(state)
+        os.sleep(loop)
+    end
+else
+    showHolo(state)
+end
+
 
 
 
