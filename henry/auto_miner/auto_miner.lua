99cd9b8d2b8c40a0ba4dadc654172b364b81734e
henryrabbit <673345280@qq.com>
2019-07-28 10:00:20 +0800

    alpha0.1
    这是一个勉强可用测试版本
    ---参与测试遇到BUG导致的损失概不负责

--- henry/auto_miner/auto_miner.lua
+++ henry/auto_miner/auto_miner.lua
@@ -3,25 +3,25 @@
 -- 原理上避免了死循环。
 -- 建议为机器人装备3*3*3模式的谐振电钻
 -- 放置机器人时请确认机器人面朝北方，并正确输入初始x,y,z坐标参数，（后续版本将增加初始化检测）
-
+dofile("auto_miner.cfg")
 local component = require("component")
 -- using openOS
 local robot = require("robot")
 local sides = require("sides")
+local computer = require("computer")
 local inv = component.inventory_controller
-local shell = require("shell")
-local s = shell.parse(...)
-local baselocation = {tonumber(s[1]),tonumber(s[2]),tonumber(s[3])}
-local location = {tonumber(s[1]),tonumber(s[2]),tonumber(s[3])}
+
+local location = {baselocation[1], baselocation[2], baselocation[3]}
+print(location[1],location[2],location[3])
+
 --北西南东的坐标向量
 local forward_vector = {{0,-1},{-1,0},{0,1},{1,0}}
 --当前面对方向
 local forward = 1
 
-local lowerbound = max(-analyze_depth, 6-ore_depth)
+local lowerbound = math.max(-analyze_depth, 6-ore_depth)
 local upperbound = analyze_depth
 
-dofile("auto_miner.cfg")
 
 --试图向某个方向先挖掘后移动并返回是否成功
 local function move( str )
@@ -38,9 +38,9 @@ local function move( str )
 	end end
 	if flag then
 		if str=="down" then 
-			y = y-1;
+			location[2] = location[2] - 1
 		else if str=="up" then
-			y = y+1;
+			location[2] = location[2] + 1
 		else 
 			location[1] = location[1] + forward_vector[forward][1]
 			location[3] = location[3] + forward_vector[forward][2]
@@ -77,22 +77,22 @@ end
 
 --躲避障碍走向指定的x,z坐标
 local function move_xz( x, z )
-	if x!=location[1] then
+	if x~=location[1] then
 		while forward_vector[forward][1]*(x-location[1])<=0 do
 			turn()
 		end
-		while x!=location[1] do
+		while x~=location[1] do
 			if move("forward")==false then
 				move("up")
 			end
 			get_charge()
 		end
 	end
-	if z!=location[3] then
+	if z~=location[3] then
 		while forward_vector[forward][2]*(z-location[3])<=0 do
 			turn()
 		end
-		while z!=location[3] do
+		while z~=location[3] do
 			if move("forward")==false then
 				move("up")
 			end
@@ -102,12 +102,13 @@ local function move_xz( x, z )
 end
 
 --汇报地牢坐标？？
-local function find_dungeon
-
+local function find_dungeon()
+ 
 end
 
 local function ore_scan( x, z, t)
-	local ans = analyzer.scan(x,z)
+	print("scanning",x,z,t)
+	local ans = component.geolyzer.scan(x,z)
 	for i = 2, t do
 		local tmp = component.geolyzer.scan(x,z)
 		for j = 33+lowerbound, 33+upperbound do
@@ -121,11 +122,11 @@ local function ore_scan( x, z, t)
 		ans[j] = ans[j]/t
 		if 4.5<ans[j] and ans[j]<5.5 then
 			numore = numore+1
-			minore = min(minore, j)
-			maxore = max(maxore, j)
+			minore = math.min(minore, j)
+			maxore = math.max(maxore, j)
 		else if 3.5<ans[j] and ans[j]<4.5 then
 			numore = numore-2
-		else if ans[j]>90000
+		else if ans[j]>90000 then
 			numore = numore-100000
 		end end end
 	end
@@ -169,17 +170,81 @@ end
 
 --往回丢产物
 local function send_ore()
+	robot.swingDown()
+	while robot.detectDown() do
+		move("forward")
+		robot.swingDown()
+	end
 	robot.select(orebox_slot)
-	swing_move("forward")
-	
+	robot.placeDown()
+	for i=1,16 do
+		if ore_slot[i] then
+			robot.select(i)
+			if inv.getStackInInternalSlot(i) then
+				for j = 1,inv.getStackInInternalSlot(i).size do
+					robot.dropDown()
+				end
+			end
+		end
+	end
+	robot.select(orebox_slot)
+	robot.swingDown()
 end
 
 --判断并拿电池充电
 local function get_charge()
-	if computer.energy()>10000 then
+	robot.select(empty_slot)
+	inv.equip()
+	local tmp = inv.getStackInInternalSlot(empty_slot).Energy
+	inv.equip()
+	robot.select(ore_slot[1])
+	if computer.energy()>10000 and tmp>200000 then
 		return
 	end
-	
+	robot.swingUp()
+	robot.swingDown()
+	while robot.detectUp() or robot.detectDown() do
+		move("forward")
+		robot.swingUp()
+		robot.swingDown()
+	end
+	robot.select(powerbox_slot)
+	robot.placeDown()
+	robot.select(empty_slot)
+	robot.suckDown()
+	robot.select(powerbox_slot)
+	robot.swingDown()
+	robot.select(empty_slot)
+	robot.placeDown()
+	move("up")
+	robot.select(charger_slot)
+	robot.placeDown()
+	robot.select(empty_slot)
+	inv.equip()
+	robot.dropDown()
+	os.sleep(5)
+	robot.suckDown()
+	inv.equip()
+	robot.select(charger_slot)
+	robot.swingDown()
+	robot.select(poweraccepter_slot)
+	robot.placeDown()
+	component.redstone.setOutput(sides.down,15)
+	while computer.energy()<=20000 do
+		os.sleep(10)
+	end
+	component.redstone.setOutput(sides.down,0)
+	robot.swingDown()
+	move("down")
+	robot.select(empty_slot)
+	robot.swingDown()
+	robot.select(powerbox_slot)
+	robot.placeDown()
+	robot.select(empty_slot)
+	robot.dropDown()
+	robot.select(powerbox_slot)
+	robot.swingDown()
+	robot.select(ore_slot[1])
 end
 
 --初始化(不必要)，通过放置方块并检测确定前方方向并标记,通过下降检测下方基岩确定高度，xz使用相对坐标系,可初始化。
